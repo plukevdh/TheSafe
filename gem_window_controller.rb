d4fe5fab5b2bf52a8646ff6ac41acfa264899541
Luke van der Hoeven <hungerandthirst@gmail.com>
2009-10-12 22:37:29 -0400

    added GCD functionality to areas needing to run asynchronusly.

--- gem_window_controller.rb
+++ gem_window_controller.rb
@@ -14,19 +14,21 @@ class GemWindowController < NSWindowController
 	attr_accessor :gemTableView
 		#inputs for adding a gem
 	attr_accessor :add_name, :add_version, :add_source, :add_docs
-		#outputs for
+		#outputs for info screen
 	attr_accessor :info_name, :info_curr_ver, :info_vers, :gem_nums
 		#segement controller
 	attr_accessor :segmentControl
-	
+		#progress indicator
 	attr_accessor :progressIndicate
 	
 	def awakeFromNib
-		get_all_gems()
-		
-		gemTableView.doubleAction = "info"
+		get_all_gems
+				
 		@gem_nums.stringValue = @gem_nums.stringValue.sub("x", @gems.size.to_s)
 		@progressIndicate.usesThreadedAnimation = true
+		
+		@gem_action_q = Dispatch::Queue.new('com.apple.gem_action_queue')
+		@semaphore = Dispatch::Semaphore.new(2)
 	end
 	
   def windowWillClose(sender)
@@ -35,7 +37,7 @@ class GemWindowController < NSWindowController
 	
 	def get_all_gems
 		@gems = []
-	
+		
 		Gem.cache.each do |gem|
 			name = gem[1].name
 			version = gem[1].version.to_s
@@ -43,10 +45,10 @@ class GemWindowController < NSWindowController
 		end
 
 		@gems.sort!
-		puts @gems.inspect
 
 		@gem_nums.stringValue = @gem_nums.stringValue.sub("/\d+/", @gems.size.to_s)
 		@gemTableView.dataSource = self
+		@gemTableView.reloadData
 	end
 	
 	def info(sender)
@@ -87,38 +89,47 @@ class GemWindowController < NSWindowController
 		
 	def update
 		select_name = @gems[@gemTableView.selectedRow].name
-		NSTask.launchedTaskWithLaunchPath("/usr/local/bin/macgem", arguments: ["update", select_name])
-		output = `gem update #{select_name}`
-		puts output
-		
-		get_all_gems
+	  @gem_action_q.dispatch do
+			@semaphore.signal
+			progressIndicate.startAnimation(sender)
+			NSTask.launchedTaskWithLaunchPath("/usr/local/bin/macgem", arguments: ["update", select_name])
+			output = `gem update #{select_name}`
+			puts output
+			progressIndicate.stopAnimation(sender)
+			get_all_gems
+		end
 	end
 	
 	def added_gem(sender)
-		gem_name = @add_name.stringValue
-    args = ['install', gem_name]
-    if @add_docs.stringValue == "1"
-      args << "--no-rdoc"
-      args << "--no-ri"
-    end 
-    args << "--version \"<= #{@add_version.stringValue}\"" unless @add_version.stringValue.empty?
-		args << "--source #{@add_source.stringValue}" unless @add_source.stringValue.empty?
-		
-		progressIndicate.startAnimation(sender)
-		
-    task = NSTask.alloc.init
-    task.launchPath = "/usr/local/bin/macgem"
-    task.arguments = args
-    task.launch
-    task.waitUntilExit
-    puts task.terminationStatus
-				
-		puts "running gem #{args.join(' ')}"
-		
-		progressIndicate.stopAnimation(sender)
+	  @gem_action_q.dispatch do
+			@semaphore.signal
+
+			gem_name = @add_name.stringValue
+			args = ['install', gem_name]
+			if @add_docs.stringValue == "1"
+				args << "--no-rdoc"
+				args << "--no-ri"
+			end 
+			args << "--version \"<= #{@add_version.stringValue}\"" unless @add_version.stringValue.empty?
+			args << "--source #{@add_source.stringValue}" unless @add_source.stringValue.empty?
 			
-		get_all_gems
-		@gemTableView.reloadData
+			progressIndicate.startAnimation(sender)
+			
+			task = NSTask.alloc.init
+			task.launchPath = "/usr/local/bin/macgem"
+			task.arguments = args
+			task.launch
+			task.waitUntilExit
+			puts task.terminationStatus
+			
+			progressIndicate.stopAnimation(sender)
+				
+			get_all_gems
+			@gemTableView.reloadData
+			
+			puts "running gem #{args.join(' ')}"
+		end
+		
 		close_add
 	end
 
@@ -139,7 +150,13 @@ class GemWindowController < NSWindowController
 	def remove
 		puts "remove a gem"
 		select_name = @gems[@gemTableView.selectedRow].name
-		NSTask.launchedTaskWithLaunchPath("/usr/local/bin/macgem", arguments: ["uninstall", select_name])
+	  @gem_action_q.dispatch do
+			@semaphore.signal
+
+			progressIndicate.startAnimation(sender)
+			NSTask.launchedTaskWithLaunchPath("/usr/local/bin/macgem", arguments: ["uninstall", select_name])
+			progressIndicate.stopAnimation(sender)
+		end
 	end
 	
 	def segmentAction(sender)
